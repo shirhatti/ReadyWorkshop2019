9f29e09c93c8880da03c6d849bdf304bde62fde8
Sourabh Shirhatti <shirhatti@outlook.com>
2019-02-02 00:14:53 -0800

    HttpContext is not Threadsafe

---
+++ demo/src/HttpContextBackground/HostedService.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HttpContextBackground
+{
+    public class MessageQueue
+    {
+        public BlockingCollection<HttpContext> Collection { get; } = new BlockingCollection<HttpContext>(100);
+
+        public virtual void EnqueueMessage(HttpContext context)
+        {
+            if (!Collection.IsAddingCompleted)
+            {
+                try
+                {
+                    Collection.Add(context);
+                    return;
+                }
+                catch (InvalidOperationException) { }
+            }
+        }
+    }
+    public class CustomHostedService : HostedService
+    {
+        private readonly MessageQueue _messageQueue;
+        public CustomHostedService(MessageQueue messageQueue)
+        {
+            _messageQueue = messageQueue;
+        }
+        protected async override Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            while(!cancellationToken.IsCancellationRequested)
+            {
+                await Task.Yield();
+                try
+                {
+                    foreach (var httpContext in _messageQueue.Collection.GetConsumingEnumerable(cancellationToken))
+                    {
+                        Console.WriteLine($"Request path: {httpContext.Request.Path}\tRequest ID: {httpContext.TraceIdentifier}\n");
+                    }
+                }
+                catch
+                {
+                    try
+                    {
+                        _messageQueue.Collection.CompleteAdding();
+                    }
+                    catch { }
+                }
+            }
+        }
+    }
+
+    public abstract class HostedService : IHostedService
+    {
+        private Task _executingTask;
+        private CancellationTokenSource _cts;
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            // Create a linked token so we can trigger cancellation outside of this token's cancellation
+            _cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+
+            // Store the task we're executing
+            _executingTask = ExecuteAsync(_cts.Token);
+
+            // If the task is completed then return it, otherwise it's running
+            return _executingTask.IsCompleted ? _executingTask : Task.CompletedTask;
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            // Stop called without start
+            if (_executingTask == null)
+            {
+                return;
+            }
+
+            // Signal cancellation to the executing method
+            _cts.Cancel();
+
+            // Wait until the task completes or the stop token triggers
+            await Task.WhenAny(_executingTask, Task.Delay(-1, cancellationToken));
+
+            // Throw if cancellation triggered
+            cancellationToken.ThrowIfCancellationRequested();
+        }
+
+        // Derived classes should override this and execute a long running method until 
+        // cancellation is requested
+        protected abstract Task ExecuteAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ demo/src/HttpContextBackground/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace HttpContextBackground
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            using (var factory = new CustomWebApplicationFactory<Startup>())
+            {
+                var client = factory.CreateDefaultClient();
+                for (var i = 0; i < 100; i++)
+                {
+                    _ = await client.GetAsync("/");
+                    //await Task.Delay(TimeSpan.FromSeconds(1));
+                }
+            }
+        }
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+
+    public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup: class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseSolutionRelativeContentRoot(@"src\HttpContextBackground");
+            builder.ConfigureLogging(logBuilder => logBuilder.ClearProviders());
+        }
+    }
+}
