3894c38cbb11c8cb871040b70385adc5cab1a2ac
Sourabh Shirhatti <shirhatti@outlook.com>
2019-02-14 09:33:54 -0800

    Double container build

---
+++ demo/src/DoubleContainerBuild/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace DoubleContainerBuild
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureServices(s => s.AddSingleton<SingleService>())
+                .UseStartup<Startup>();
+    }
+
+    public class SingleService
+    {
+        public Guid Id { get; set; }
+        public SingleService()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+
+    public class Startup
+    {
+        public Startup(SingleService service)
+        {
+            Console.WriteLine(service.Id);
+        }
+
+        public void Configure(IApplicationBuilder app, SingleService service)
+        {
+            Console.WriteLine(service.Id);
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
